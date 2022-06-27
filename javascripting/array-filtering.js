d0529e23f005e6d6df2fb474a9cb6088c7fe1866
Luisa <63885146+StaryLu@users.noreply.github.com>
2022-06-27 09:58:14 -0300

    Add files via upload

--- javascripting/array-filtering.js
+++ javascripting/array-filtering.js
@@ -1,1 +1,6 @@
-const numbers = [1,2,3,4,5,6,7,8,9,10]
+function evenNumbers(numbers){
+    return numbers%2==0
+}
+ numbers = [1,2,3,4,5,6,7,8,9,10]
+filtered=numbers.filter(evenNumbers)
+console.log(filtered)
